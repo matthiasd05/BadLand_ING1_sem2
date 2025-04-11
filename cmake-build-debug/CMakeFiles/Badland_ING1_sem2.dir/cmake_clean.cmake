9de5fbada088e28fe8801a4033f8b7c6236730bd
matthiasd05 <matthias.defruit@edu.ece.fr>
2025-04-11 13:48:22 +0200

    1er kommit

---
+++ cmake-build-debug/CMakeFiles/Badland_ING1_sem2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "Badland_ING1_sem2.exe"
+  "Badland_ING1_sem2.exe.manifest"
+  "Badland_ING1_sem2.pdb"
+  "CMakeFiles/Badland_ING1_sem2.dir/fonction.c.obj"
+  "CMakeFiles/Badland_ING1_sem2.dir/fonction.c.obj.d"
+  "CMakeFiles/Badland_ING1_sem2.dir/main.c.obj"
+  "CMakeFiles/Badland_ING1_sem2.dir/main.c.obj.d"
+  "libBadland_ING1_sem2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Badland_ING1_sem2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Badland_ING1_sem2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Badland_ING1_sem2.
+# This may be replaced when dependencies are built.
